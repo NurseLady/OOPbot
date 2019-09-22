4d642016deb9fd1104738b6075e70c39aea69149
Meanvile <55655138+Meanvile@users.noreply.github.com>
2019-09-22 21:45:02 +0500

    Update ConsoleApp.java
    Взаимодействие с файлом MessageHandler.java, ввод данных для пользователя.

--- src/ConsoleApp.java
+++ src/ConsoleApp.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class ConsoleApp {
-    public static void main() {
-        System.out.println("Привет^_^! Напиши мне что-нибудь.");
+    public static void Main() {
         MessageHandler chat = new MessageHandler();
+        chat.GetStartMessage();
         while (true){
+            Scanner in = new Scanner(System.in);
+            String message = in.next();
+            chat.HandleMessage(message);
         }
     }
 }
