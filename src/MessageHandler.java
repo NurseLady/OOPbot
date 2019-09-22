726f6fa6f0f50465459ccd4d32d29e643e903afe
NurseLady <svetazay96@gmail.com>
2019-09-22 17:50:26 +0500

    Update MessageHandler.java
    Добавлен метод, отправляющий приветственное сообщение

--- src/MessageHandler.java
+++ src/MessageHandler.java
@@ -1,5 +1,10 @@
 public class MessageHandler {
     public static String HandleMessage(String message){
         return message;
+    private IGameMode game;
+
+    public String GetStartMessage(){
+        return "Привет! Я бот, с которым можно поиграть. Начать игру? (y/n)";
+    }
     }
 }
