71ad5857352d2243d2094f8defc5829734889192
NurseLady <svetazay96@gmail.com>
2019-09-22 17:52:55 +0500

    Update MessageHandler.java
    Добавлена логика обработки сообщений пользователя и взаимодействия с игрой

--- src/MessageHandler.java
+++ src/MessageHandler.java
@@ -1,10 +1,41 @@
+import interfaces.IGameMode;
+
 public class MessageHandler {
-    public static String HandleMessage(String message){
-        return message;
     private IGameMode game;
 
     public String GetStartMessage(){
         return "Привет! Я бот, с которым можно поиграть. Начать игру? (y/n)";
     }
+
+    public String HandleMessage(String message){
+        message = message.toLowerCase().replaceAll(System.getProperty("line.separator"), " ").trim();
+        
+        if (game != null){
+            switch (message){
+                case "s":
+                    return "Игра началась!\n" +
+                            "Если вопрос слишком сложный, отправь \"n\" и я его пропущу.\n" +
+                            "Когда игра надоест, отправь \"f\" и она закончится.\n\n" + game.GetQuestion();
+                case "f":
+                    game = null;
+                    return "Игра окончена. Хочешь поиграть ещё? (y/n)";
+                case "n":
+                    return game.Skip();
+                default:
+                    return game.CheckUserAnswer(message);
+            }
+        }
+
+        switch (message){
+            case "n":
+                return "Ок, если передумаешь напиши \"y\".";
+
+            case "y":
+                game = new SimplestGameMod();
+                return this.HandleMessage("s");
+
+            default:
+                return "Я таких слов не знаю!";
+        }
     }
 }
