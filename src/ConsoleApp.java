6ea546f55cd9cc6e1fc70d4db6d8fecc91796fd0
Meanvile <55655138+Meanvile@users.noreply.github.com>
2019-09-22 17:22:01 +0500

    Update ConsoleApp.java

--- src/ConsoleApp.java
+++ src/ConsoleApp.java
@@ -1,4 +1,4 @@
-  public class ConsoleApp {
+public class ConsoleApp {
   public static void main() {
     System.out.println("Привет^_^! Напиши мне что-нибудь.");
     MessageHandler chat = new MessageHandler();
