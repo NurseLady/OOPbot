8c82142f4bef82153a9d36327dc752a1dc9f54f2
NurseLady <svetazay96@gmail.com>
2019-09-22 17:52:47 +0500

    Update SimplestGameMod.java
    Добавлены заготовки методов для реализации интерфейса

--- src/SimplestGameMod.java
+++ src/SimplestGameMod.java
@@ -1,4 +1,18 @@
 import interfaces.IGameMode;
 
 public class SimplestGameMod implements IGameMode {
+    @Override
+    public String GetQuestion() {
+        return null;
+    }
+
+    @Override
+    public String CheckUserAnswer(String message) {
+        return null;
+    }
+
+    @Override
+    public String Skip() {
+        return null;
+    }
 }
