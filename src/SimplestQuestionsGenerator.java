eca46264f5da6ca4e9f67aaf6e0a754db0646715
Meanvile <55655138+Meanvile@users.noreply.github.com>
2019-09-23 11:38:38 +0500

    Update SimplestQuestionsGenerator.java

--- src/SimplestQuestionsGenerator.java
+++ src/SimplestQuestionsGenerator.java
@@ -1,4 +1,11 @@
 import interfaces.IQuestionsGenerator;
+import java.util.*;
+import java.io.*;
 
-public class SimplestQuestionsGenerator implements IQuestionsGenerator{
-}
+public class SimplestQuestionsGenerator implements IQuestionsGenerator {
+    public int question;
+    public int GenerateQuestion(){
+        return  question = new Random().nextInt(100);
+    }
+    public int GenerateAnswer(){return question;}
+    public int GenerateBait(){return new Random().nextInt(100);}
