7fc5bd196043d83688276c59ad19a6d28d7bdeaf
NurseLady <svetazay96@gmail.com>
2019-09-23 10:28:22 +0500

    Update SimplestGameMod.java
    В принципе, игра готова и ждёт только генератор вопросов

--- src/SimplestGameMod.java
+++ src/SimplestGameMod.java
@@ -1,18 +1,28 @@
 import interfaces.IGameMode;
+import interfaces.IQuestionsGenerator;
 
 public class SimplestGameMod implements IGameMode {
+    private IQuestionsGenerator generator = new SimplestQuestionsGenerator();
+    private Question quest;
+
+
     @Override
     public String GetQuestion() {
-        return null;
+        quest = generator.GenerateQuestion();
+        return quest.toString();
     }
 
     @Override
     public String CheckUserAnswer(String message) {
-        return null;
+
+        if (message == quest.GetCorrectAnswerNumber().toString()){
+            return "И это правильный ответ! " + this.Skip();
+        }
+        return "Не верно, попробуй ещё разок!";
     }
 
     @Override
     public String Skip() {
-        return null;
+        return "Хорошо, держи следующий вопрос: \n\n" + this.GetQuestion();
     }
 }
