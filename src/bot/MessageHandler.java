b7768840e83dd2ec43bb7f795541e75299da2618
NurseLady <svetazay96@gmail.com>
2019-10-06 20:14:32 +0500

    MessageHandler refactor

--- src/bot/MessageHandler.java
+++ src/bot/MessageHandler.java
@@ -2,9 +2,9 @@ package bot;
 
 import interfaces.GameMode;
 
-class MessageHandler {
+public class MessageHandler {
     private GameMode game;
-    String startMessage = "Привет! Я бот, с которым можно поиграть. Начать игру? (y/n)";
+    public String startMessage = "Привет! Я бот, с которым можно поиграть. Начать игру? (y/n)";
     private String gameStartMessage = "Игра началась!\n" +
             "Если вопрос слишком сложный, отправь \"n\" и я его пропущу.\n" +
             "Когда игра надоест, отправь \"f\" и она закончится.\n\n";
@@ -12,7 +12,7 @@ class MessageHandler {
     private String noGameMessage = "Отправь \"y\" если передумаешь.";
     private String incorrectInputMessage = "Я таких слов не знаю!";
 
-    String handleMessage(String message){
+    public String handleMessage(String message){
         message = message.toLowerCase().replaceAll(System.getProperty("line.separator"), " ").trim();
         String result;
         
